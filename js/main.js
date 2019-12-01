a50c339be98656a273a925bd2394031d332b6e2d
salmarizk <58375610+salmarizk@users.noreply.github.com>
2019-12-01 04:50:55 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ /*$(document).ready(function() {  
+$('li').click(function()
+{
+    $('li', $(this).parent()).removeClass('active');
+    $(this).addClass('active');
+})
+$('#features').onClick(function(){
+    $('#app-features').addClass('bounceInDown');
+});
+}) */
